38571300142bdd2142d483db4c6725aeca2c72bd
Engineer Youssef Mahmoud <youssefaboali122@gmail.com>
2026-01-23 14:17:13 +0200

    Update API title and improve logging messages

--- SSMS_Api.py
+++ SSMS_Api.py
@@ -2,20 +2,16 @@ from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 import mlflow.pyfunc
 import pandas as pd
-import numpy as np
 
-app = FastAPI(title="SMS Spam Classifier API with MLflow")
+app = FastAPI(title="SMS Spam Detection API with MLflow")
 
-# --- تحميل الموديل بحذر ---
 model_name = "Spam_Classifier"
 try:
-    # بيحاول يسحب اللي في Production
     model = mlflow.pyfunc.load_model(f"models:/{model_name}/Production")
-    print("✅ Loaded Production Model")
+    print("Loaded Production Model")
 except:
-    # لو ملقاش، بيسحب أحدث نسخة مسجلة (عشان الـ API يشتغل بأي حال)
     model = mlflow.pyfunc.load_model(f"models:/{model_name}/latest")
-    print("⚠️ Production model not found, loaded latest version instead.")
+    print(" Production model not found, loaded latest version instead.")
 
 
 class MessageInput(BaseModel):
@@ -33,16 +29,19 @@ def predict_message(input_data: MessageInput):
         res = int(prediction[0])
         label = "Spam" if res == 1 else "Ham"
 
-        # تأكد إن المسميات دي هي اللي Streamlit هينادي عليها
         return {
             "prediction": label,
-            "probability": 1.0,  # بما إن الموديل MultinomialNB مش باعت Proba حالياً
+            "probability": 1.0,
             "status": "Success"
         }
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
 
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="127.0.0.1", port=8000)
 if __name__ == "__main__":
     import uvicorn
 
