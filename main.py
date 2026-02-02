76845f3c441f7bdae615ea1ba33b1d7a295498c6
Engineer Youssef Mahmoud <youssefaboali122@gmail.com>
2026-02-02 03:24:45 +0200

    Implement logging in main.py for better output management
    Replaced print statements with logging for better traceability.

--- main.py
+++ main.py
@@ -4,7 +4,10 @@ import eda_text
 import visualization
 import model_training
 import mlflow_lifecycle
-
+import logging
+from logger_config import setup_logging
+setup_logging()
+logger = logging.getLogger(__name__)
 
 def main():
     parser = argparse.ArgumentParser(description="Spam Classifier MLflow Project")
@@ -14,22 +17,22 @@ def main():
 
     args = parser.parse_args()
 
-    print(" Starting Spam Classifier End-to-End Pipeline...")
-    print("=" * 60)
-    print(f"Current Settings -> Alpha: {args.alpha}, Max Features: {args.max_features}")
-    print("=" * 60)
+    logger.info(" Starting Spam Classifier End-to-End Pipeline...")
+    logger.info("=" * 60)
+    logger.info(f"Current Settings -> Alpha: {args.alpha}, Max Features: {args.max_features}")
+    logger.info("=" * 60)
 
     DATA_PATH = r"C:\Users\Hedaya_city\Downloads\spam.csv"
-    print("\n>>> Step 1: Loading and Cleaning Raw Data")
+    logger.info("\n>>> Step 1: Loading and Cleaning Raw Data")
     df_cleaned = data_pipeline.prepare_data(DATA_PATH)
 
-    print("\n>>> Step 2: Text Preprocessing and EDA")
+    logger.info("\n>>> Step 2: Text Preprocessing and EDA")
     df_processed, corr_matrix = eda_text.perform_eda_and_pre(df_cleaned)
 
-    print("\n>>> Step 3: Generating Visualizations")
+    logger.info("\n>>> Step 3: Generating Visualizations")
     visualization.run_visualizations(df_processed, corr_matrix)
 
-    print("\n>>> Step 4: Training MultinomialNB Model")
+    logger.info("\n>>> Step 4: Training MultinomialNB Model")
     training_results = model_training.train_and_evaluate_model(
         df_processed,
         alpha_val=args.alpha,
@@ -37,14 +40,14 @@ def main():
     )
 
 
-    print("\n>>> Step 5: Logging to MLflow & Model Registry")
+    logger.info("\n>>> Step 5: Logging to MLflow & Model Registry")
     run_id = mlflow_lifecycle.run_mlflow_lifecycle(training_results)
 
-    print("\n" + "=" * 60)
-    print(f" Full Project Execution Completed Successfully!")
-    print(f" MLflow Run ID: {run_id}")
-    print(f" Tracking UI: Run 'mlflow ui' to see the results.")
-    print("=" * 60)
+    logger.info("\n" + "=" * 60)
+    logger.info(f" Full Project Execution Completed Successfully!")
+    logger.info(f" MLflow Run ID: {run_id}")
+    logger.info(f" Tracking UI: Run 'mlflow ui' to see the results.")
+    logger.info("=" * 60)
 
 if __name__ == "__main__":
     main()
